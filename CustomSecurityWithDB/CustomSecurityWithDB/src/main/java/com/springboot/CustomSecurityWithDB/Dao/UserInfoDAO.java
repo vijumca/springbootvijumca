7471737d197e4a323e36001cb7f7645f362d2e0f
vijumca <vijumca@gmail.com>
2020-05-07 22:35:05 +0530

    move

--- CustomSecurityWithDB/CustomSecurityWithDB/src/main/java/com/springboot/CustomSecurityWithDB/Dao/UserInfoDAO.java
+++
@@ -1,7 +0,0 @@
-package com.springboot.CustomSecurityWithDB.Dao;
-
-import com.springboot.CustomSecurityWithDB.entities.UserInfo;
-
-public interface UserInfoDAO {
-	public abstract UserInfo getActiveUser(String userName);
-}
